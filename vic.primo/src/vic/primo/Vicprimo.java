3ac6ceb8db7d7cbc28ada16b92c830e9c07fbb2b
x491 <vic.makara@gmail.com>
2021-06-11 10:26:43 +0200

    prova

---
+++ vic.primo/src/vic/primo/Vicprimo.java
@@ -0,0 +1,32 @@
+package vic.primo;
+
+public class Vicprimo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=1;
+		System.out.println(a/b);
+		System.out.println((double)a/b);
+		double c= (double) a;
+		System.out.println(c);
+		
+		boolean m = true;
+		System.out.println(m);
+		
+		if (a<b) {
+			System.out.println(m);
+		}
+		
+		else if (a>b) {
+			System.out.println("a è maggiore di b");
+		}
+		else {
+			System.out.println("a è uguale a b");
+		}
+	
+		
+
+	}
+
+}
