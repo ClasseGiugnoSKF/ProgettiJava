25f524b0d76282b1655006852c284fb321fec027
FloatBit64 <esp.nico@outlook.it>
2021-06-15 10:27:26 +0200

    Esercizio 15 Giugno

---
+++ Esercizio 15 Giugno Ricerca array/src/Avvio.java
@@ -0,0 +1,10 @@
+
+public class Avvio {
+
+	public static void main(String[] args) {
+		Metodi m = new Metodi();
+		String[] nominativi = m.creaVettoreStringa();
+		m.ricercaNome(nominativi);
+	}
+
+}
---
+++ Esercizio 15 Giugno Ricerca array/src/Metodi.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Metodi {
+	Scanner input = new Scanner(System.in);
+
+	String leggiStringa(String messaggio) {
+		System.out.println(messaggio);
+		String stringa = input.nextLine();
+		return stringa;
+	}
+	
+	int leggiIntero(String messaggio) {
+		System.out.println(messaggio);
+		int intero = Integer.parseInt(input.nextLine());
+		return intero;
+	}
+	
+	String[] creaVettoreStringa() {
+		String[] vettore = new String[leggiIntero("Inserisci la dimensione del vettore: ")];
+		for(int i=0;i<vettore.length;i++) {
+			System.out.println("Inserisci nominativo n."+(i+1)+":");
+			vettore[i]=input.nextLine();
+		}
+		return vettore;
+	}
+	
+	void ricercaNome(String[] vettore) {
+		String nome = leggiStringa("Chi vuoi cercare nell'array?");
+		boolean risultato = true;
+		for(int i=0;i<vettore.length;i++) {
+			if(vettore[i].equalsIgnoreCase(nome)) {
+				System.out.println(nome+" è stato trovato!");
+				break;
+			} else {
+				risultato = false;
+			}
+		}
+		if(!risultato) {
+			System.out.println(nome+" non trovato");
+		}
+	}
+}
