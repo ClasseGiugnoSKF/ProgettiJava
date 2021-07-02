a48e196db81fad73177d4276fa0b4d4959fcdf77
Mirko Onorato <85618159+mirkoSkF@users.noreply.github.com>
2021-07-02 16:37:53 +0200

    Add files via upload

---
+++ Giochiamo con le espressioni Lambda/src/Cerchio.java
@@ -0,0 +1,16 @@
+
+public class Cerchio extends Figura2D {
+
+	@Override
+	public Double calcArea() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double calcPerim() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Giochiamo con le espressioni Lambda/src/Figura2D.java
@@ -0,0 +1,11 @@
+
+public abstract class Figura2D {
+	
+	public Double area;
+	public Double perim;
+	
+	public abstract Double calcArea();
+	
+	public abstract Double calcPerim();
+
+}
